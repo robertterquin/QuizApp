ebd8f183acc994ad1a3bde8fceb3fcbeb57f6bc8
Robert Terquin Laqui <terquinlaqui@gmail.com>
2025-03-23 15:00:29 +0800

    Updates

--- app/src/main/java/com/example/quizapp/splash_screen.java
+++ app/src/main/java/com/example/quizapp/splash_screen.java
@@ -24,6 +24,7 @@ public class splash_screen extends AppCompatActivity {
             return insets;
         });
 
+
         TextView registerBtn = findViewById(R.id.register_button);
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
