a1e0d2fc4cad29d3fcd9b9f60a35c50013b74973
Robert Terquin Laqui <terquinlaqui@gmail.com>
2025-04-21 13:38:09 +0800

    Changes

--- app/src/main/java/com/example/quizapp/CustomAdapter.java
+++ app/src/main/java/com/example/quizapp/CustomAdapter.java
@@ -8,9 +8,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.quizapp.Item;
-import com.example.quizapp.R;
-
 import java.util.ArrayList;
 
 public class CustomAdapter extends ArrayAdapter<Item> {
@@ -18,7 +15,7 @@ public class CustomAdapter extends ArrayAdapter<Item> {
     private final Activity context;
     private final ArrayList<Item> itemList;
 
-    public CustomAdapter(Activity context, ArrayList<Item> itemList) {
+    public CustomAdapter(MainActivity context, ArrayList<Item> itemList) {
         super(context, R.layout.custom_item, itemList);
         this.context = context;
         this.itemList = itemList;
@@ -29,15 +26,12 @@ public class CustomAdapter extends ArrayAdapter<Item> {
         LayoutInflater inflater = context.getLayoutInflater();
         View itemView = inflater.inflate(R.layout.custom_item, null, true);
 
-        // Find views
         ImageView itemImage = itemView.findViewById(R.id.item_image);
         TextView itemTitle = itemView.findViewById(R.id.item_title);
         TextView itemDescription = itemView.findViewById(R.id.item_description);
 
-        // Get current item
         Item currentItem = itemList.get(position);
 
-        // Set data
         itemImage.setImageResource(currentItem.getImageResource());
         itemTitle.setText(currentItem.getTitle());
         itemDescription.setText(currentItem.getDescription());
